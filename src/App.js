c4796cecc2b5825050d26e5ff4a0d886fa3b3fb6
Kauê Oliveira <kaueoliveira993@gmail.com>
2025-05-01 17:12:37 -0300

    Estudando sobre renderização condicional

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React, { useState } from "react";
-import Led from "./components/Led";
+import React, {useState} from "react";
+
 
 export default function App() {
-  const [ligado, setLigado] = useState(false);
+
+  const [cor, setCor] = useState(1)
+
+  const vermelho = {color: '#f00'}
+  const verde = {color: '#0f0'}
+  const azul = {color: '#00f'}
+
+  const retCor =(c)=> {
+    if (c === 1) {
+      return vermelho
+    } else if (c === 2) {
+      return verde
+    } else {
+      return azul
+    }
+  }
+
+  const mudaCor =()=> {
+    console.log(cor)
+    setCor(cor+1)
+    if (cor > 2) {
+      setCor(1)
+    }
+  }
+  
   return (
     <>
-      <Led ligado={ligado} setLigado={setLigado}/>    
+      <h1 style={retCor(cor)}>CFB Cursos</h1>
+      <button onClick={()=>mudaCor()}>Mudar Cor</button>  
     </>
   );
 }
