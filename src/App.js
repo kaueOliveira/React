40bafc3b874834a906f8c313b866a55803e040a8
Kauê Oliveira <kaueoliveira993@gmail.com>
2025-05-06 19:29:42 -0300

    Resolução do Exercício

--- src/App.js
+++ src/App.js
@@ -3,18 +3,28 @@ import Nota from "./components/Nota";
 import Resultado from "./components/Resultado";
 
 export default function App() {
-  const [nota1, setNota1] = useState(0);
-  const [nota2, setNota2] = useState(0);
-  const [nota3, setNota3] = useState(0);
-  const [nota4, setNota4] = useState(0);
+  const [notas, setNotas] = useState({"nota1":0, "nota2":0, "nota3":0, "nota4":0,})
 
+  const handleSetNotas =(e)=> {
+    console.log(e.target)
+    if (e.target.getAttribute('name') === 'nota1') {
+      setNotas({"nota1": e.target.value, "nota2": notas.nota2, "nota3" : notas.nota3, "nota4": notas.nota4})
+    } else if (e.target.getAttribute('name') === 'nota2') {
+      setNotas({"nota1": notas.nota1, "nota2": e.target.value, "nota3" : notas.nota3, "nota4": notas.nota4})
+    } else if (e.target.getAttribute('name') === 'nota3') {
+      setNotas({"nota1": notas.nota1, "nota2": notas.nota2, "nota3" : e.target.value, "nota4": notas.nota4})
+    } else {
+      setNotas({"nota1": notas.nota1, "nota2": notas.nota2, "nota3" : notas.nota3, "nota4": e.target.value})
+    }
+  }
+  
   return (
     <>
-      <Nota num={1} nota={nota1} setNota={setNota1}/>
-      <Nota num={2} nota={nota2} setNota={setNota2}/>
-      <Nota num={3} nota={nota3} setNota={setNota3}/>
-      <Nota num={4} nota={nota4} setNota={setNota4}/>
-      <Resultado somaNota={parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3) + parseFloat(nota4)}/>
+      <Nota num={1} nota={notas.nota1} setNota={handleSetNotas}/>
+      <Nota num={2} nota={notas.nota2} setNota={handleSetNotas}/>
+      <Nota num={3} nota={notas.nota3} setNota={handleSetNotas}/>
+      <Nota num={4} nota={notas.nota4} setNota={handleSetNotas}/>
+      <Resultado somaNota={parseFloat(notas.nota1) + parseFloat(notas.nota2) + parseFloat(notas.nota3) + parseFloat(notas.nota4)}/>
     </>
   );
 }
--- src/components/Nota.js
+++ src/components/Nota.js
@@ -4,7 +4,7 @@ export default function Nota(props) {
     return (
         <div>
             <legend>Informe a nota: {props.num}</legend>
-            <input type='text' value={props.nota} onChange={(e)=> props.setNota(e.target.value)}/>
+            <input type='text' name={'nota' + props.num} value={props.nota} onChange={(e)=> props.setNota(e)}/>
         </div>
     );
 }
