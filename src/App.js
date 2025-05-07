c6c5cb26e770b8bcb99ba688cd50238145d98282
Kauê Oliveira <kaueoliveira993@gmail.com>
2025-05-07 20:36:09 -0300

    Estudando sobre LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
-
-  const [contagem, setContagem]=useState(0)
-
-  useEffect(
-    ()=>{
-      console.log("Página Carregada")
-      document.title="Contagem" + contagem
-    }
-  )
   
+  const [nome, setNome]=useState()
+
+  const armazenar = (chave, valor) => {
+    localStorage.setItem(chave, valor)
+  }
+
+  const consutar = (chave) => {
+    alert(localStorage.getItem(chave))
+  }
+
+  const apagar = (chave) => {
+    localStorage.removeItem(chave)
+  }
+
   return (
     <>
-      <p>Contagem: {contagem}</p>
-      <button onClick={()=>setContagem(contagem+1)}>Contar</button>
-    </>
+      <label>Digite um Nome</label><br/>
+      <input type="text" value={nome} onChange={(e)=>setNome(e.target.value)}/><br/>
+      <button onClick={()=>armazenar("ls_nome", nome)}>Gravar Nome</button>
+      <button onClick={()=>consutar("ls_nome")}>Ver Nome</button>
+      <button onClick={()=>apagar("ls_nome")}>Remover Nome</button>
+    </> 
   );
 }
 
